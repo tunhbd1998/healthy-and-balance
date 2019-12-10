bd1a27ccdb341705cee9c68a6dd784106e3ed087
Nguyễn Văn Phước <phuocnv2@vng.com.vn>
2019-12-10 13:28:32 +0700

    add label

--- src/components/commons/label/index.js
+++ src/components/commons/label/index.js
@@ -1,17 +1,11 @@
 import React from "react";
-import "../button/button.styles.scss";
-
-const BUTTON_TYPE = {
-  PRIMARY: "primary",
-  DANGER: "danger",
-  SECONDARY: "secondary",
-};
+import {Badge} from 'react-bootstrap';
+import './label.css';
 
 export default function Label({ children, type }) {
   return (
-    <div
-      className={`hb-button hb-button-${type || BUTTON_TYPE.PRIMARY}`}>
+    <Badge variant={type} className="lb">
       {children}
-    </div>
+    </Badge>
   );
 }
