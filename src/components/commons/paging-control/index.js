a8ddcb2b0452a78a7c55e6b6f0650d8f5427f149
Nguyễn Văn Phước <phuocnv2@vng.com.vn>
2019-12-10 20:04:47 +0700

    hover paging button

--- src/components/commons/paging-control/index.js
+++ src/components/commons/paging-control/index.js
@@ -7,15 +7,15 @@ import './paging-control.scss';
 export default function PagingControl({ onNext, onPrev, currentPage, maxPage }) {
     let buttonNext, buttonPrev;
     if (currentPage === 1) {
-        buttonPrev = (<Button className="button-pg button-disable"><FontAwesomeIcon  icon={faAngleLeft}/></Button>);
+        buttonPrev = (<Button className="button-pg button-disable"><icon><FontAwesomeIcon  icon={faAngleLeft}/></icon></Button>);
     } else {
-        buttonPrev = (<Button className="button-pg" onClick={onPrev}><FontAwesomeIcon  icon={faAngleLeft} /></Button>);
+        buttonPrev = (<Button className="button-pg" onClick={onPrev}><icon><FontAwesomeIcon  icon={faAngleLeft} /></icon></Button>);
     }
 
     if (currentPage === maxPage) {
-        buttonNext = (<Button className="button-pg button-disable"><FontAwesomeIcon  icon={faAngleRight} /></Button>);
+        buttonNext = (<Button className="button-pg button-disable"><icon><FontAwesomeIcon  icon={faAngleRight} /></icon></Button>);
     } else {
-        buttonNext = (<Button className="button-pg" onClick={onNext}><FontAwesomeIcon  icon={faAngleRight} /></Button>);
+        buttonNext = (<Button className="button-pg" onClick={onNext}><icon><FontAwesomeIcon  icon={faAngleRight} /></icon></Button>);
     }
 
     return (
