8853f68e4cd8b2b1fa46c13f5b4eb8b120a1a533
Nguyễn Văn Phước <phuocnv2@vng.com.vn>
2019-12-12 00:31:39 +0700

    change color title

--- src/components/commons/dialog/index.js
+++ src/components/commons/dialog/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 import Button from "../button";
-import "./dialog.styles.css";
+import "./dialog.styles.scss";
 
 export default function Dialog({
   title,
